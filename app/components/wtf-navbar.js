d8a714e496f0173f98611624bef1b29641353a0b
Dan Knutsen <dknutsen@gmail.com>
2019-01-16 13:42:48 -0600

    pics and stuff

--- app/components/wtf-navbar.js
+++ app/components/wtf-navbar.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  toggleNav: false,
+
+  actions: {
+    toggleNav() {
+      this.toggleProperty('toggleNav');
+    }
+  }
 });
